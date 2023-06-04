c1b0c4f685f192bc2d4b27201886880e201e4d60
alexi <chitch@alxi.nl>
2023-06-04 11:26:32 -0400

    upgrade version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -19,9 +19,6 @@ git checkout main
 # git merge private
 git merge --no-ff --no-commit private
 
-# Replace "http://127.0.0.1:8000/" with "https://api.getjuno.ai/" in all files in the juno/js/src directory
-find ./juno/js/src -type f -exec sed -i 's|http://127.0.0.1:8000/|https://api.getjuno.ai/|g' {} \;
-
 # Remove agent.js
 if git rev-parse --verify --quiet juno/js/src >/dev/null; then
     git reset HEAD -- juno/js/src
@@ -49,6 +46,11 @@ fi
 if git rev-parse --verify --quiet juno/js/package-lock.json >/dev/null; then
     git rm juno/js/package-lock.json
 fi
+
+# Replace "http://127.0.0.1:8000/" with "https://api.getjuno.ai/" in all files in the juno/js/src directory
+sed -i '' 's|http://127.0.0.1:8000/|https://api.getjuno.ai/|g' ./juno/js/juno.min.js
+
+git add ./juno/js/juno.min.js
 
 # Commit the changes
 git commit -m "$commit_message"
